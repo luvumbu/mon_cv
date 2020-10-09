ae5443739c07ddedaeaca3e62dfb94a1a32592da
Luvumbu <luvumbu.n@gmail.com>
2020-10-09 23:40:59 +0200

    preparation de la connexion en bdd

--- class/php/connexion.php
+++ class/php/connexion.php
@@ -1,5 +1,17 @@
 <?php
+session_start();
 header("Access-Control-Allow-Origin: *");
-$servername = "localhost";
- echo "Connexion ok " ; 
-?>
+$REMOTE_ADDR= $_SERVER['REMOTE_ADDR']; 
+if($REMOTE_ADDR=="::1"){
+  $servername = "localhost";
+  $username = "root";
+  $password = $username;
+  $dbname = "all_ffa";
+
+}
+else {
+  $servername = "localhost";
+  $username = "u481158665_all_ffa3";
+  $password ="v3p9r3e@59A";
+  $dbname = $username;
+}
