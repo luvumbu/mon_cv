db8a4cfa7e5c6b2e00f3e27c4bb86f967cd5a87d
Luvumbu <luvumbu.n@gmail.com>
2020-10-12 22:10:10 +0200

    ad function ok

--- login/vue.js
+++ login/vue.js
@@ -1,11 +1,10 @@
 var body = new Vue({
   el: '#body',
   data: {
-    statue_menu:false 
+    statue_menu: false
   },
   methods: {
-    action: function () {
-    },
+    action: function () {},
     navbarNavAltMarkup_change: function () {
       document.getElementById("navbarNavAltMarkup").className = "block";
     },
@@ -17,37 +16,41 @@ var body = new Vue({
     },
     home: function () {
       this.all_header();
+      this.toggle_menu();
       document.getElementById("home").className = "nav-item nav-link active";
     },
     add_information: function () {
       this.all_header();
+      this.toggle_menu();
       document.getElementById("add_information").className = "nav-item nav-link active";
     },
     my_model: function () {
       this.all_header();
+      this.toggle_menu();
       document.getElementById("my_model").className = "nav-item nav-link active";
     },
     my_conf: function () {
       this.all_header();
+      this.toggle_menu();
       document.getElementById("my_conf").className = "nav-item nav-link active";
     },
-    navbarNavAltMarkup:function(){
-      if(this.statue_menu==false){
-        document.getElementById("navbarNavAltMarkup").style="display:block;text-align:center"; 
-        this.statue_menu=true;
-      }
-      else{
-        document.getElementById("navbarNavAltMarkup").style="display:none;"; 
-        this.statue_menu=false;
-      }
- 
-
-},
+    navbarNavAltMarkup: function () {
+      this.toggle_menu(); 
+    },
     all_header: function () {
       document.getElementById("home").className = "nav-item nav-link";
       document.getElementById("add_information").className = "nav-item nav-link";
       document.getElementById("my_model").className = "nav-item nav-link";
       document.getElementById("my_conf").className = "nav-item nav-link";
+    },
+    toggle_menu: function () {
+      if (this.statue_menu == false) {
+        document.getElementById("navbarNavAltMarkup").style = "display:block;text-align:center";
+        this.statue_menu = true;
+      } else {
+        document.getElementById("navbarNavAltMarkup").style = "display:none;";
+        this.statue_menu = false;
+      }
     }
   }
 })
