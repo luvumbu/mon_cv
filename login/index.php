71db484137c52525cc578d5ea11d2f2e62278db1
Luvumbu <luvumbu.n@gmail.com>
2020-10-13 02:48:00 +0200

    ajout du icone header

--- login/index.php
+++ login/index.php
@@ -4,7 +4,6 @@ include("class/php/ip.php");
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,21 +14,18 @@ include("class/php/ip.php");
 	?>
 	<title>Bokonzi</title>
 </head>
-
 <body>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<div id="body">
 		<header>
 			<?php
 			if (isset($_SESSION["information"])) {
 
-				if ($_SESSION["information"] == "Succes") { 
-				
-				}
-				else {
+				if ($_SESSION["information"] == "Succes") {
+				} else {
 					echo ' <meta http-equiv="refresh" content="0; URL=../index.php" />';
 				}
-			}
-			else {
+			} else {
 				echo ' <meta http-equiv="refresh" content="0; URL=../index.php" />';
 			}
 
@@ -50,20 +46,20 @@ include("class/php/ip.php");
 		include("vue.html");
 		?>
 	</div>
-
 	<style>
-		.nav-link:hover{
+		.nav-link:hover {
 			cursor: pointer;
 		}
-		.block{
-			display:block;
-			text-align:center;			
-		} 
-	 .ok{
-		 text-align: center;
-		 display: block;
 
-	 }
+		.block {
+			display: block;
+			text-align: center;
+		}
+
+		.ok {
+			text-align: center;
+			display: block;
+		}
 	</style>
 </body>
 
